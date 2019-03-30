40ac45f62c8bde5a6661c0374932b5e96245b002
Malcolm Leake <echelon1089@gmail.com>
2019-03-30 22:32:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   @songs << song
   song.artist = self
   @@all_songs += 1 
-  binding.pry
+  #binding.pry
   end
  
  def add_song_by_name(song_name)
@@ -30,6 +30,8 @@ class Artist
   song.artist = self
   @songs << song
   @@all_songs += 1
+  binding.pry
+  
  end  
   
   def self.song_count
